b9912cfa37b7084422862b3068d3ca52cc238fb1
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-30 15:55:39 -0300

    Update catalogo.py

--- backend/catalogo.py
+++ backend/catalogo.py
@@ -10,22 +10,56 @@ CATALOGO = {
         "campos_extras": []              # nenhum
     },
     "99Taxi": {
-        "adm": "JFL ADMINISTRADORA IMOBILIÁRIA JML LTDA.",
+        "adm": "JFL Administradora Imobiliária JML Ltda.",
         "adm_cnpj": "43.883.992/0001-55",
-        "spe": "SPE II JFL JML747 EMPREENDIMENTO IMOBILIARIO S.A.",
+        "spe": "SPE II JFL JML747 Empreendimento Imobiliario S.A.",
         "spe_cnpj": "51.920.614/0001-22",
         "endereco": "José Maria Lisboa St, 747",
         "idioma": "en",
         "campos_extras": ["doc", "case_code"]   # passaporte + case code
     },
     "Bain": {
-        "adm": "JFL FARIA LIMA ADMINISTRADORA IMOBILIÁRIA LTDA.",
+        "adm": "JFL Faria Lima Administradora Imobiliária Ltda.",
         "adm_cnpj": "47.427.424/0001-09",
-        "spe": "SPE JFL FARIA LIMA S.A.",
+        "spe": "SPE JFL Faria Lima S.A.",
         "spe_cnpj": "XX.XXX.XXX/0001-YY",
         "endereco": "Av. Brigadeiro Faria Lima, 3477",
         "idioma": "pt",                  # recibo em português
         "campos_extras": ["case_code"]
     },
-    # …adicione outros…
+    "Acciona": {
+        "adm": "JFL Faria Lima Administradora Imobiliária Ltda.",
+        "adm_cnpj": "47.427.424/0001-09",
+        "spe": "SPE JFL Faria Lima S.A.",
+        "spe_cnpj": "XX.XXX.XXX/0001-YY",
+        "endereco": "Av. Brigadeiro Faria Lima, 3477",
+        "idioma": "pt",                  # recibo em português
+        "campos_extras": ["case_code"]
+    },
+    "Mitsui": {
+        "adm": "JFL Faria Lima Administradora Imobiliária Ltda.",
+        "adm_cnpj": "47.427.424/0001-09",
+        "spe": "SPE JFL Faria Lima S.A.",
+        "spe_cnpj": "XX.XXX.XXX/0001-YY",
+        "endereco": "Av. Brigadeiro Faria Lima, 3477",
+        "idioma": "pt",                  # recibo em português
+        "campos_extras": ["case_code"]
+    },
+    "Netcracker": {
+        "adm": "JFL Faria Lima Administradora Imobiliária Ltda.",
+        "adm_cnpj": "47.427.424/0001-09",
+        "spe": "SPE JFL Faria Lima S.A.",
+        "spe_cnpj": "XX.XXX.XXX/0001-YY",
+        "endereco": "Av. Brigadeiro Faria Lima, 3477",
+        "idioma": "pt",                  # recibo em português
+        "campos_extras": ["case_code"]
+    },
+    "IMM": {
+        "adm": "JFL Faria Lima Administradora Imobiliária Ltda.",
+        "adm_cnpj": "47.427.424/0001-09",
+        "spe": "SPE JFL Faria Lima S.A.",
+        "spe_cnpj": "XX.XXX.XXX/0001-YY",
+        "endereco": "Av. Brigadeiro Faria Lima, 3477",
+        "idioma": "pt",                  # recibo em português
+        "campos_extras": ["case_code"]
 }
