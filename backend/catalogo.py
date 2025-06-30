b3d01c8bc9e93b386ea636462f6176660bc771ec
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-30 10:54:51 -0300

    Create catalogo.py

---
+++ backend/catalogo.py
@@ -0,0 +1,31 @@
+# catalogo.py  (arquivo novo)
+CATALOGO = {
+    "Huawei": {          # chave curta que vamos mandar no <select>
+        "adm": "JFL Administradora Imobiliária Nações Unidas Ltda.",
+        "adm_cnpj": "49.826.447/0001-03",
+        "spe": "SPE JFL AVNU Empreendimento Imobiliario S.A.",
+        "spe_cnpj": "35.946.965/0001-56",
+        "endereco": "Av. Nações Unidas, 15.187",
+        "idioma": "en",                  # recibo sempre inglês
+        "campos_extras": []              # nenhum
+    },
+    "99Taxi": {
+        "adm": "JFL ADMINISTRADORA IMOBILIÁRIA JML LTDA.",
+        "adm_cnpj": "43.883.992/0001-55",
+        "spe": "SPE II JFL JML747 EMPREENDIMENTO IMOBILIARIO S.A.",
+        "spe_cnpj": "51.920.614/0001-22",
+        "endereco": "José Maria Lisboa St, 747",
+        "idioma": "en",
+        "campos_extras": ["doc", "case_code"]   # passaporte + case code
+    },
+    "Bain": {
+        "adm": "JFL FARIA LIMA ADMINISTRADORA IMOBILIÁRIA LTDA.",
+        "adm_cnpj": "47.427.424/0001-09",
+        "spe": "SPE JFL FARIA LIMA S.A.",
+        "spe_cnpj": "XX.XXX.XXX/0001-YY",
+        "endereco": "Av. Brigadeiro Faria Lima, 3477",
+        "idioma": "pt",                  # recibo em português
+        "campos_extras": ["case_code"]
+    },
+    # …adicione outros…
+}
